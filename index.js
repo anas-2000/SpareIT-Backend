b2c5e645d51b00c47fae52a5918d26b4c5a28159
khanmohsin10 <khanmkpmohsin10@gmail.com>
2023-04-05 01:59:11 +0500

    created separate collections for different types of users, fixed login, added seller stats

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const express = require("express")
 const passport = require('passport')
 const session = require('express-session');
@@ -45,4 +46,53 @@ app.use(passport.session())
 
 app.listen(process.env.PORT, () => {
     console.log(`listening on port ${process.env.PORT}...`)
+=======
+const express = require("express")
+const passport = require('passport')
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const cors = require("cors")
+const dotenv = require("dotenv")
+
+const userRoute = require("./routes/user")
+const authRoute = require("./routes/auth")
+const productRoute = require("./routes/product")
+const cartRoute = require("./routes/cart")
+const orderRoute = require("./routes/order")
+const stripeRoute = require("./routes/stripe")
+const db = require("./db")
+
+
+
+const app = express()
+dotenv.config()
+
+require('./passport')(passport)
+
+db()
+app.use(express.json())
+
+app.use(cors())
+app.use("/api/auth", authRoute)
+app.use("/api/users", userRoute)
+app.use("/api/products", productRoute)
+app.use("/api/carts", cartRoute)
+app.use("/api/orders", orderRoute)
+app.use("/api/stripe", stripeRoute)
+
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {secure: false}, // this won't work without https
+    store: MongoStore.create({ mongoUrl: process.env.MONGO_URL })
+  }));
+app.use(passport.initialize())
+app.use(passport.session())
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`listening on port ${process.env.PORT}...`)
+>>>>>>> edf23a2 (latest commit)
 })
